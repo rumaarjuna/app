416645ec671a44bd411836afd4c8ab3c13a7132f
rumaarjuna <rumaarjuna@gmail.com>
2026-08-18 18:10:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
   Jangan simpan API key Gemini/Groq di sini.
 */
 window.RUMA_CONFIG = {
-  API_URL: "PASTE_APPS_SCRIPT_EXEC_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbwsWLNR7NB2MQTe173VQPfRM3nf1rpg5pqoNk2btgUfPq3YvSe463FmLE8xNLpDNgaI/exec",
   APP_VERSION: "2.0.0",
   REQUEST_TIMEOUT: 25000,
   PWA_ENABLED: true
