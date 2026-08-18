d27f070b5a0b3b9d74778c73a7548e9cca2e864f
rumaarjuna <rumaarjuna@gmail.com>
2026-08-18 14:55:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  * If left blank, RUMA will ask for it on first launch and store it on this device.
  */
 window.RUMA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxKd9YURKD4eLtbijSV8M_FuoVTiMhB-WyV0Ut70I1L36g8XIOe8o2nQGPIFbMar-ev/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzuOA4eePfRxKtiQb5vS7h2KYvnu6IICja5_M1OI0fQuRGQBenNJam5xjYBkEWVETgl/exec",
   APP_VERSION: "1.0.0"
 };
