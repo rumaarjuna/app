4094b7fd0a38ba55f6162314170476a9107899dc
rumaarjuna <rumaarjuna@gmail.com>
2026-08-19 11:14:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ruma-pwa-v2.6.2';
+const CACHE='ruma-pwa-v2.7.0';
 const STATIC=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
