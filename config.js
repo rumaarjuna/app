8e5ff262a266b214555661105d4c3eb23867cc04
rumaarjuna <rumaarjuna@gmail.com>
2026-09-12 13:11:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,15 @@ window.RUMA_CONFIG = {
   }
 };
 
- // URL Web App project RUMA Notify Service BARU:
+/*
+ * Tambahkan NOTIFY_API_URL ke window.RUMA_CONFIG yang SUDAH ada.
+ * Jangan menaruh FONNTE_TOKEN di file ini.
+ */
+window.RUMA_CONFIG = {
+  // URL backend utama RUMA yang sudah digunakan saat ini:
+  API_URL: 'https://script.google.com/macros/s/AKfycbwsWLNR7NB2MQTe173VQPfRM3nf1rpg5pqoNk2btgUfPq3YvSe463FmLE8xNLpDNgaI/exec',
+
+  // URL Web App project RUMA Notify Service BARU:
   NOTIFY_API_URL: 'https://script.google.com/macros/s/AKfycbzkwt5phMJWg3a_Bn2CeokTGwHFgVm89pt7efS80i-1SDfd0cW0v4rx1hiIq8c73o3k/exec'
 };
 
